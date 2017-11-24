38fd4ba9859beb63fefed2dc1aa0af8c71186190
murkymao <glaciefer.1@gmail.com>
2017-11-24 13:55:01 +0200

    change the paths to assets

--- page.php
+++ page.php
@@ -11,18 +11,18 @@
     <title>Freelancer - Start Bootstrap Theme</title>
 
     <!-- Bootstrap core CSS -->
-    <link href="<?php  ?>vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="<?php bloginfo('template_directory') ?>/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Custom fonts for this template -->
-    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="<?php bloginfo('template_directory') ?>/vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css">
 
     <!-- Plugin CSS -->
-    <link href="vendor/magnific-popup/magnific-popup.css" rel="stylesheet" type="text/css">
+    <link href="<?php bloginfo('template_directory') ?>/vendor/magnific-popup/magnific-popup.css" rel="stylesheet" type="text/css">
 
     <!-- Custom styles for this template -->
-    <link href="css/freelancer.min.css" rel="stylesheet">
+    <link href="<?php bloginfo('template_directory') ?>/css/freelancer.min.css" rel="stylesheet">
 
 </head>
 
@@ -399,19 +399,19 @@
 </div>
 
 <!-- Bootstrap core JavaScript -->
-<script src="vendor/jquery/jquery.min.js"></script>
-<script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="<?php bloginfo('template_directory') ?>/vendor/jquery/jquery.min.js"></script>
+<script src="<?php bloginfo('template_directory') ?>/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
 <!-- Plugin JavaScript -->
-<script src="vendor/jquery-easing/jquery.easing.min.js"></script>
-<script src="vendor/magnific-popup/jquery.magnific-popup.min.js"></script>
+<script src="<?php bloginfo('template_directory') ?>/vendor/jquery-easing/jquery.easing.min.js"></script>
+<script src="<?php bloginfo('template_directory') ?>/vendor/magnific-popup/jquery.magnific-popup.min.js"></script>
 
 <!-- Contact Form JavaScript -->
-<script src="js/jqBootstrapValidation.js"></script>
-<script src="js/contact_me.js"></script>
+<script src="<?php bloginfo('template_directory') ?>/js/jqBootstrapValidation.js"></script>
+<script src="<?php bloginfo('template_directory') ?>/js/contact_me.js"></script>
 
 <!-- Custom scripts for this template -->
-<script src="js/freelancer.min.js"></script>
+<script src="<?php bloginfo('template_directory') ?>/js/freelancer.min.js"></script>
 
 </body>
 
